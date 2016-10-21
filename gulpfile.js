28cc4124e3c37714a59835d2b76504b7e0c0cbbb
byukin <bogdan@MacBook-Pro.local>
2016-10-21 22:20:23 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    styl = require('gulp-stylus'),
+    stylus = require('gulp-stylus'),
     jade = require('gulp-jade'),
     autoprefixer = require('gulp-autoprefixer'),
     shorthand = require('gulp-shorthand'),
@@ -9,9 +9,27 @@ var gulp = require('gulp'),
 	cssmin = require('gulp-cssmin'),
 	rename = require('gulp-rename');
 
+var path = {
+    master: { //куда складывать готовые после сборки файлы
+        html: 'master/',
+        js: 'master/js/',
+        css: 'master/style/css/',
+        img: 'master/style/img/',
+        fonts: 'master/style/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        css: 'src/style/css/style.styl',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+
+};
+
 
 gulp.task('webserver', ['watch'], function() {
-  gulp.src('./')
+  gulp.src('./master')
     .pipe(webserver({
       livereload: true,
       directoryListing: true,
@@ -20,11 +38,11 @@ gulp.task('webserver', ['watch'], function() {
 });
 
 
-gulp.task('styl', function () {
-    gulp.src('./src/style.styl')
-        .pipe(styl())
+gulp.task('stylus', function () {
+    gulp.src(path.src.css)
+        .pipe(stylus())
         .on('error', console.log)
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(path.master.css))
         .pipe(livereload());
 });
 
@@ -33,83 +51,61 @@ gulp.task('shorthand', function () {
     gulp.src('./css/style.css')
         .pipe(shorthand())
         .on('error', console.log)
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('master/style/css'))
         .pipe(livereload());
 });
 
 gulp.task('cssmin', function () {
-    gulp.src('./css/style.css')
+    gulp.src('./master/style/css/style.css')
         .pipe(cssmin())
 		.on('error', console.log)
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('master/style/css'));
 });
 
 
 
 gulp.task('jade', function () {
-    gulp.src('./src/index.jade')
+    gulp.src('./src/*.jade')
         .pipe(jade({
               pretty: true
             }))
         .on('error', console.log)
-        .pipe(gulp.dest(''))
+        .pipe(gulp.dest('master'))
         .pipe(livereload());
 });
 
 gulp.task('autoprefixer', function () {
-    gulp.src('./css/style.css')
+    gulp.src('./master/style/css/style.css')
          .pipe(autoprefixer({
             browsers: ['last 10 versions'],
             cascade: false
         }))
         .on('error', console.log)
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('master/style/css'))
         .pipe(livereload());
 });
 
 gulp.task('base64', function () {
-    gulp.src('./css/style.css')
+    gulp.src('./master/style/css/style.css')
          .pipe(base64({
-            baseDir: "./img",
+            baseDir: "./master/style/img",
             extensionsAllowed: ['.gif', '.png']
         }))
         .on('error', console.log)
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('master/style/css'))
         .pipe(livereload());
 });
 
-gulp.task('htmlbeautify', function() {
-  var options = {
-    indentSize: 2
-  };
-  gulp.src('./*.html')
-    .pipe(htmlbeautify(options))
-    .pipe(gulp.dest('./'))
-});
 
 
 
 gulp.task('watch', function(){
     livereload.listen();
     gulp.watch('./src/*.jade',['jade']);
-    gulp.watch('./src/*.styl',['styl']);
+    gulp.watch('./src/*.styl',['stylus']);
     gulp.watch('./css/*.css',['autoprefixer']);
 	gulp.watch('./css/style.css',['cssmin']);
 });
 
-gulp.task('gitinit', function(){
-  git.init(function (err) {
-    if (err) throw err;
-  });
-});
-gulp.task('addremote', function(){
-  git.addRemote('origin', 'https://github.com/BogdanYukin/vector.git', function (err) {
-    if (err) throw err;
-  });
-});
-gulp.task('clone', function(){
-  git.clone('https://github.com/BogdanYukin/vector.git', function (err) {
-    if (err) throw err;
-  });
-});
+
