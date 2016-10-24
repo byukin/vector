af4e6413a276117da9a9f71763812fc4215b4157
byukin <bogdan@MacBook-Pro.local>
2016-10-24 22:58:58 +0300

    +

--- master/js/main.js
+++ master/js/main.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
- 		$(".mainslider").owlCarousel({
-			navigation : true, // Show next and prev buttons
-			slideSpeed : 400,
-			paginationSpeed : 400,
-			singleItem:true,
-			navigationText:["",""],
-			autoHeight : true,
-				  // "singleItem:true" is a shortcut for:
-				  // items : 1,
-				  // itemsDesktop : false,
-				  // itemsDesktopSmall : false,
-				  // itemsTablet: false,
-				  // itemsMobile : false
-		});
+ 	$(".slider").owlCarousel({
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 400,
+		paginationSpeed : 400,
+		singleItem:true,
+		navigationText:["",""],
+		autoHeight : true,
+	});
+
+	function hidemenuitem() {
+		var w = $( window ).width();
+			if (w < 1150) {
+				$('.link_hide').html('Печати');
+			} else {
+				$('.link_hide').html('Печати и штампы');
+			}
+	}
+
+	hidemenuitem();
+
+	$(window).resize(function(){
+		hidemenuitem();
+	});
+
 
 });
