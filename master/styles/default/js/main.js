3d9913586aedd993377da1565bbe3afc84abd7ab
BogdanYukin <b.yukin2@gmail.com>
2016-10-26 14:45:24 +0300

    +

--- master/styles/default/js/main.js
+++ master/styles/default/js/main.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
- 	$(".slider").owlCarousel({
-		navigation : true, // Show next and prev buttons
-		slideSpeed : 400,
-		paginationSpeed : 400,
-		singleItem:true,
-		navigationText:["",""],
-		autoHeight : true,
-	});
+ 	appSite.init();
+});
 
-	function hidemenuitem() {
+
+var appSite = {
+	init: function() {
+		appSite.sliderBind($(".slider"));
+		appSite.hideMenuItemBind();
+	},
+	hideMenuItem: function () {
 		var w = $( window ).width();
 			if (w < 1150) {
 				$('.link_hide').html('Печати');
 			} else {
 				$('.link_hide').html('Печати и штампы');
 			}
+	},
+	hideMenuItemBind: function() {
+		appSite.hideMenuItem();
+
+		$(window).resize(function(){
+			appSite.hideMenuItem();
+		});
+	},
+	sliderBind: function(rElem) {
+		rElem.owlCarousel({
+			navigation : true, // Show next and prev buttons
+			slideSpeed : 400,
+			paginationSpeed : 400,
+			singleItem:true,
+			navigationText:["",""],
+			autoHeight : true,
+		});
+		rElem.css('opacity','1');
 	}
-
-	hidemenuitem();
-
-	$(window).resize(function(){
-		hidemenuitem();
-	});
-
-
-});
+};
