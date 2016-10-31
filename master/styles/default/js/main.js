a97c47c8d6f10926c9e3260436d61fb99bdeccd4
BogdanYukin <b.yukin2@gmail.com>
2016-10-31 14:57:03 +0300

    +

--- master/styles/default/js/main.js
+++ master/styles/default/js/main.js
@@ -7,6 +7,8 @@ var appSite = {
 	init: function() {
 		appSite.sliderBind($(".slider"));
 		appSite.hideMenuItemBind();
+		appSite.activeMenuItemBind($(".menu"));
+		appSite.activeTabItemBind($(".page-content-tabs a"));
 	},
 	hideMenuItem: function () {
 		var w = $( window ).width();
@@ -33,5 +35,37 @@ var appSite = {
 			autoHeight : true,
 		});
 		rElem.css('opacity','1');
-	}
+	},
+	activeMenuItemBind: function(mElem) {
+		var path = window.location.pathname.slice(1);
+		mElem.find("a[href='"+path+"']").addClass('active')
+	},
+	activeTabItemBind: function(mElem) {
+		mElem.click(function(){
+			appSite.activeTabItemBindClick($(this), mElem);
+			return false;
+		});
+	},
+	activeTabItemBindClick: function(mElem,rlElem) {
+		var sUri = mElem.attr('href');
+
+		$.get(sUri,
+				function (sResult, sStatus, sHead) {
+					appSite.activeTabUpdateHtml(sResult);
+					rlElem.removeClass('active');
+					mElem.addClass('active');
+				}).error(
+				function (sStatus) {
+					// appSite.errorLoad(sStatus);
+				}
+			);
+
+	},
+	activeTabElem: function() {
+		return '.page-content-text';
+	},
+	activeTabUpdateHtml: function (sResult) {
+		var sHtml = $(appSite.activeTabElem(), sResult).html();
+		$(appSite.activeTabElem()).html(sHtml);
+	},
 };
